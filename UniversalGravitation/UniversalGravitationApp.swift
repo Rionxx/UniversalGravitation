e3bb8c9831ae4f546f2c4bbd849cf9878ec1af11
Rion <rionfx.42@gmail.com>
2022-01-22 22:07:25 +0900

    first commit

---
+++ UniversalGravitation/UniversalGravitationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalGravitationApp.swift
+//  UniversalGravitation
+//
+//  Created by FX on 2022/01/22.
+//
+
+import SwiftUI
+
+@main
+struct UniversalGravitationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
