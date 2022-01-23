145565ec4f969d4357a47aa35d4be1935ea0510c
Rion <rionfx.42@gmail.com>
2022-01-23 11:10:58 +0900

    Create Caluculate function in HomeView

--- UniversalGravitation/Views/ContentView.swift
+++ UniversalGravitation/Views/ContentView.swift
@@ -8,33 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var price = ""
-    @State private var tax = ""
-    @State private var totalSum = 0
-    @State private var finalSum = 0
-    
     var body: some View {
-        VStack {
-            TextField("価格", text: self.$price)
-                .frame(width: 150, height: 50)
-                .keyboardType(.default)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            
-            TextField("税", text: self.$tax)
-                .frame(width: 150, height: 50)
-                .keyboardType(.default)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            
-            Button(action: {
-                self.totalSum = Int(Double(self.price)! + Double(self.price)! * Double(self.tax)! / 100.0)
-                self.finalSum = Int(self.totalSum)
-            }) {
-                Text("計算!")
-            }
-            
-            Text("Sum: \(finalSum)")
-        }
+        HomeView()
     }
 }
 
--- UniversalGravitation/Views/HomeView.swift
+++ UniversalGravitation/Views/HomeView.swift
@@ -13,26 +13,53 @@ struct HomeView: View {
     @State private var M = ""
     @State private var m = ""
     @State private var radius = ""
-    var coefficient = 6.67 * pow(10.0, -11.0)
-    var Force = 0
+    @State private var coefficient: Double = 6.67 * pow(10.0, -11.0)
+    @State private var Force = 0.0
+    @State private var finalForce = 0.0
     
     var body: some View {
         VStack {
-            Text("Hello, World!")
+            Text("万有引力の法則")
+                .font(.title)
+                .padding(.bottom, 20)
+            
+            VStack {
+                TextField("Mの重さ", text: self.$M)
+                TextField("mの重さ", text: self.$m)
+                TextField("半径", text: self.$radius)
+            }
+            .frame(width: 200, height: 100)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.bottom, 30)
+            
             Button(action: {
-                
+                self.Force = self.coefficient * (Double(self.M)! * Double(self.m)!) / (Double(self.radius)! * Double(self.radius)!)
+                self.finalForce = Double(self.Force)
             }) {
                 Text("Let's Caluclate!")
+            }
+            
+            Button(action: {
+                clear()
+            }) {
+                Text("Clear")
+                    .foregroundColor(Color.red)
             }.padding(.bottom, 20)
             
             Text("万有引力 \(Force)")
             Image("image")
                 .resizable()
                 .frame(width: 200, height: 200)
+            Spacer()
         }
     }
     
-    //万有引力の関数
+    func clear() {
+        self.M = ""
+        self.m = ""
+        self.radius = ""
+        self.Force = 0.0
+    }
 }
 
 struct HomeView_Previews: PreviewProvider {
